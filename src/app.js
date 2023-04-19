103138394247d9d061b7d0b924b0b1e20308f0e3
Thiana <thianacantunes@gmail.com>
2023-04-19 11:24:28 -0300

    Requisito de cadastro implementado com sucesso

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import joi from "joi"
+import bcrypt from "bcrypt"
 
 //Criação do Servidor
 const app = express();
@@ -15,11 +17,59 @@ dotenv.config()
 let db
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
 mongoClient.connect()
-    .then(() => db = mongoClient.db())
+    .then(() => {
+        db = mongoClient.db()
+        console.log("Mongodb rodando normalmente")})
     .catch((err) => console.log(err.message))
+
+// Shermas
+
+ const cadastroUsuario = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    senha: joi.string().required().min(3),
+    confsenha: joi.string().required().min(3)
+ })
 
 // Endpoint
 
+app.post("/cadastro", async (req, res) => {
+    const {name, email, senha, confsenha} = req.body
+
+    const senhaCript = bcrypt.hashSync(senha, 10)
+
+    const validate = cadastroUsuario.validate(req.body, {abortEarly: false});
+
+    if(validate.error){
+        const errors = validate.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try{
+
+       const usuarioExistente = await db.collection("infoUsuarios").findOne({email})
+
+       if(usuarioExistente){
+        return res.status(409).send("Email já cadastrado anteriormente")
+       }
+
+    }catch(err){
+        res.sendStatus(500)
+    }
+
+    if(senha !== confsenha){
+        return res.status(409).send("Confirmação de senha não confere com a senha")
+    }
+
+
+    try {
+        await db.collection("infoUsuarios").insertOne({name, email, senha: senhaCript})
+        return res.status(201).send("Usuário cadastrado com sucesso")
+    } catch (err){
+        res.sendStatus(500)
+    }
+
+})
 
 
 
